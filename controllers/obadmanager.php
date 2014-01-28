dba7e276c834e1ae0edf6c194aa523368c26f33b
Mike Gagnon <mike@it-mike.com>
2014-01-28 15:45:33 -0700

    feature complete, testing needed.

--- controllers/obadmanager.php
+++ controllers/obadmanager.php
@@ -6,14 +6,14 @@ class ObAdManager extends OBFController
   public function __construct()
   {
     parent::__construct();
-    $this->user->require_authenticated();
+    // $this->user->require_authenticated();
     $this->model = $this->load->model('ObAdManager');
   }
 
   public function get_settings()
   {
     $this->user->require_permission('ad_manager_settings or ad_manager_access');
-    return array(true,'Category settings.',array('enabled'=>$this->model->get_enabled(),'disabled'=>$this->model->get_disabled()));
+    return array(true,'Category settings.',array('enabled'=>$this->model->get_enabled(),'disabled'=>$this->model->get_disabled(),'timezone'=>$this->model->get_timezone()));
   }
 
   public function save_settings()
@@ -22,27 +22,65 @@ class ObAdManager extends OBFController
 
     $enabled_id = $this->data('enabled');
     $disabled_id = $this->data('disabled');
+    $timezone = $this->data('timezone');
 
-    if(!$this->db->id_exists('media_genres',$enabled_id) || !$this->db->id_exists('media_genres',$disabled_id))
-      return array(false,'One of the selected categories does not seem to be valid.  Please refresh OpenBroadcaster and try again.');
+    $validation = $this->model('validate_settings',$enabled_id,$disabled_id,$timezone);
 
-    if($enabled_id == $disabled_id) 
-      return array(false,'The enabled and disabled categories cannot be the same.');
+    if($validation[0]==false) return $validation;
 
-    $this->model->set_enabled($enabled_id);
-    $this->model->set_disabled($disabled_id);
-    $this->model->adjust_media();
+    $this->model('set_enabled',$enabled_id);
+    $this->model('set_disabled',$disabled_id);
+    $this->model('set_timezone',$timezone);
+    $this->model('adjust_media');
 
     return array(true,'Settings saved.');
   }
 
-  public function get_schedule()
+  public function get_items()
+  {
+    $this->user->require_permission('ad_manager_access');
+    return array(true,'Ad manager schedule.',$this->model('get_items'));
+  }
+
+  public function save_item()
   {
     $this->user->require_permission('ad_manager_access');
 
-    return array(true,'Ad manager schedule.',array(1,2,3));
+    $data = array();
+    $data['start_date'] = trim($this->data('start_date'));
+    $data['start_time'] = trim($this->data('start_time'));
+    $data['stop_date'] = trim($this->data('stop_date'));
+    $data['stop_time'] = trim($this->data('stop_time'));
+    $data['notes'] = trim($this->data('notes'));
+    $data['item_id'] = trim($this->data('item_id'));
+
+    $id = $this->data('id');
+    if(empty($id)) $id = false;
+    
+    $validate = $this->model('validate_item',$data,$id);
+    if(!$validate[0]) return $validate;
+
+    $this->model('save_item',$data,$id);
+    $this->model('adjust_media');
+
+    return array(true,'Item saved.');
   }
 
-  
+  public function delete_item()
+  {
+    $this->user->require_permission('ad_manager_access');
+    $id = $this->data('id');
+    if(!empty($id)) $this->model('delete_item',$id);
 
+    $this->model('adjust_media');
+
+    return array(true,'Item deleted.');
+  }
+
+  public function adjust_media()
+  {
+    $this->model('adjust_media');
+    return array(true,'Media adjusted.');
+  }
+  
 }
--- js/ob_ad_manager.js
+++ js/ob_ad_manager.js
@@ -1,6 +1,7 @@
-
-
 var ModuleObAdManager = new Object();
+
+
+/* Manager Page */
 
 ModuleObAdManager.init_module = function()
 {
@@ -15,19 +16,194 @@ ModuleObAdManager.load = function()
   {
     $('#layout_main').html(html.get('modules/ob_ad_manager/manager.html'));
 
-    if(!settings.data.enabled || !settings.data.disabled)
+    if(!settings.data.enabled || !settings.data.disabled || !settings.data.timezone)
     {
-      $('#ob_ad_manager_messagebox').text('The ad manager does not seem to be configured properly.  Please set categories in "admin -> ad manager settings", or contact an administrator for assistance.').show();
+      $('#ob_ad_manager_messagebox').text('The ad manager does not seem to be configured properly.  Please adjust settings in "admin -> ad manager settings", or contact an administrator for assistance.').show();
       return;
     }
 
-    api.post('obadmanager','get_schedule',{},function(response)
-    {
-      console.log(response);
+    $('#ob_ad_manager').show();
+
+    $('#ob_ad_manager_drop').droppable({
+      drop: function(event, ui) {
+
+        if($(ui.draggable).attr('data-mode')!='media') { alert('Only media items are supported.'); return; }
+
+        if($('.sidebar_search_media_selected').length!=1) { alert('You can manage only one media item at a time.'); return; }
+
+        var item_id = $('.sidebar_search_media_selected').first().attr('data-id');
+        var item_name = $('.sidebar_search_media_selected').first().attr('data-artist')+' - '+$('.sidebar_search_media_selected').first().attr('data-title');
+        var item_type = $('.sidebar_search_media_selected').first().attr('data-type');
+
+        ModuleObAdManager.add_item(item_id,item_name,item_type);
+
+      }
     });
+
+    ModuleObAdManager.get_items();
+
+  });
+}
+
+ModuleObAdManager.get_items = function()
+{
+
+  api.post('obadmanager','get_items',{},function(response)
+  {
+    if(!response.status) return;
+
+    ModuleObAdManager.items_cache = {};
+
+    $('#ob_ad_manager_expired tbody').remove();
+    $('#ob_ad_manager_current tbody').remove();
+    $('#ob_ad_manager_upcoming tbody').remove();
+
+    $('#ob_ad_manager_expired').append(ModuleObAdManager.tbody_html(response.data.expired));
+    $('#ob_ad_manager_current').append(ModuleObAdManager.tbody_html(response.data.current));
+    $('#ob_ad_manager_upcoming').append(ModuleObAdManager.tbody_html(response.data.upcoming));
+
+    if(!$('#ob_ad_manager_expired tbody').length) $('#ob_ad_manager_expired').append('<tbody><tr><td colspan="4">No ads found.</td></tr></tbody>');
+    if(!$('#ob_ad_manager_current tbody').length) $('#ob_ad_manager_current').append('<tbody><tr><td colspan="4">No ads found.</td></tr></tbody>');
+    if(!$('#ob_ad_manager_upcoming tbody').length) $('#ob_ad_manager_upcoming').append('<tbody><tr><td colspan="4">No ads found.</td></tr></tbody>');
+
+    $('#ob_ad_manager table tr.ad_item').dblclick(ModuleObAdManager.edit_item);
   });
 
 }
+
+ModuleObAdManager.tbody_html = function(items)
+{
+  if(items.length<1) return '';
+
+  $tbody = $('<tbody></tbody>');
+
+  $.each(items, function(index,item)
+  {
+    var $tr = $('<tr></tr>');
+    $tr.append('<td class="ob_ad_manager_media">'+htmlspecialchars(item.media_artist+' - '+item.media_title)+'</td>');
+    $tr.append('<td class="ob_ad_manager_start">'+item.start_date+' '+item.start_time+'</td>');
+    $tr.append('<td class="ob_ad_manager_stop">'+item.stop_date+' '+item.stop_time+'</td>');
+    $tr.append('<td>'+htmlspecialchars(item.notes)+'</td>');
+    $tbody.append('<tr class="ad_item" data-id="'+item.id+'">'+$tr.html()+'</tr>');
+
+    ModuleObAdManager.items_cache[item.id] = item;
+  }); 
+
+  return '<tbody>'+$tbody.html()+'</tbody>';
+}
+
+ModuleObAdManager.edit_item = function()
+{
+  var item = ModuleObAdManager.items_cache[$(this).attr('data-id')];
+
+  ModuleObAdManager.addedit_window();
+
+  $('#ob_ad_manager_start_date').val(item.start_date);
+  $('#ob_ad_manager_start_time').val(item.start_time);
+  $('#ob_ad_manager_stop_date').val(item.stop_date);
+  $('#ob_ad_manager_stop_time').val(item.stop_time);
+  $('#ob_ad_manager_item_id').val(item.media_id);
+  $('#ob_ad_manager_id').val(item.id);
+
+  $('#ob_ad_manager_notes').val(item.notes);
+}
+
+ModuleObAdManager.add_item = function(item_id,item_name,item_type)
+{
+
+  this.addedit_window();
+
+  $('#DOMWindow').find('.edit_only').hide();
+
+  $('#ob_ad_manager_item_id').val(item_id);
+  $('#ob_ad_manager_item_info').text(item_name+' (media #'+item_id+')');
+
+}
+
+ModuleObAdManager.save_item = function()
+{
+
+  fields = new Object();
+
+  fields.notes = $('#ob_ad_manager_notes').val();
+
+  fields.start_date = $('#ob_ad_manager_start_date').val();
+  fields.start_time = $('#ob_ad_manager_start_time').val();
+
+  fields.stop_date = $('#ob_ad_manager_stop_date').val();
+  fields.stop_time = $('#ob_ad_manager_stop_time').val();
+
+  fields.id = $('#ob_ad_manager_id').val();
+  fields.item_id = $('#ob_ad_manager_item_id').val();
+
+  $('#ob_ad_manager_addedit_messagebox').hide();
+
+  api.post('obadmanager','save_item',fields,function(data) 
+  {
+
+    if(data.status==false)
+    {
+      $('#ob_ad_manager_addedit_messagebox').text(data.msg).show();
+    }
+
+    else
+    {
+      layout.close_dom_window();
+      ModuleObAdManager.get_items();
+    }
+
+  });
+
+}
+
+ModuleObAdManager.addedit_window = function()
+{
+
+  layout.open_dom_window();
+  $('#DOMWindow').html(html.get('modules/ob_ad_manager/manager_addedit.html'));
+
+  // friendly date picker
+  $('#ob_ad_manager_start_date').datepicker({ dateFormat: "yy-mm-dd" });
+  $('#ob_ad_manager_stop_date').datepicker({ dateFormat: "yy-mm-dd" });
+
+  // friendly time picker
+  $('#ob_ad_manager_start_time').timepicker({timeFormat: 'hh:mm:ss',showSecond: true});
+  $('#ob_ad_manager_stop_time').timepicker({timeFormat: 'hh:mm:ss',showSecond: true});
+
+}
+
+ModuleObAdManager.delete_item = function(confirm)
+{
+  if(confirm)
+  {
+    api.post('obadmanager','delete_item',{ 'id': $('#ob_ad_manager_id').val() }, function(data)
+    {
+      if(data.status==true)
+      {
+        layout.close_dom_window();
+        ModuleObAdManager.get_items();
+      }
+
+      else
+      {
+        $('#ob_ad_manager_addedit_messagebox').text(data.msg);
+      }
+    });
+
+  }
+
+  else
+  {
+    $('#ob_ad_manager_addedit_messagebox').html('<p>Delete this item?</p><p><input type="button" value="Yes, Delete" onclick="ModuleObAdManager.delete_item(true);"> &nbsp; &nbsp; <input type="button" value="No, Cancel" onclick="$(\'#ob_ad_manager_addedit_messagebox\').hide();"></p>').show();
+  }
+}
+
+
+
+
+
+
+/* Settings Page */
 
 ModuleObAdManager.load_settings = function()
 {
@@ -35,6 +211,7 @@ ModuleObAdManager.load_settings = function()
   api.post('obadmanager','get_settings',{},function(response)
   {
     $('#layout_main').html(html.get('modules/ob_ad_manager/settings.html'));
+    $('#ob_ad_manager_timezone').html(html.get('device/tzoptions.html'));
 
     // fill category list
     for(var i in settings.categories)
@@ -46,6 +223,7 @@ ModuleObAdManager.load_settings = function()
     $('#ob_ad_manager_enabled_category').change(function() { ModuleObAdManager.settings_update_genre_list('enabled'); });
     $('#ob_ad_manager_disabled_category').change(function() { ModuleObAdManager.settings_update_genre_list('disabled'); });
 
+    if(response.data.timezone) $('#ob_ad_manager_timezone').val(response.data.timezone);
     if(response.data.enabled) $('#ob_ad_manager_enabled_category').val(response.data.enabled.media_category_id);
     if(response.data.disabled) $('#ob_ad_manager_disabled_category').val(response.data.disabled.media_category_id);
 
@@ -74,12 +252,13 @@ ModuleObAdManager.settings_update_genre_list = function(field)
 
 ModuleObAdManager.save_settings = function()
 {
+  var timezone = $('#ob_ad_manager_timezone').val();
   var enabled_id = $('#ob_ad_manager_enabled_genre').val();
   var disabled_id = $('#ob_ad_manager_disabled_genre').val();
 
   $('#ob_ad_manager_messagebox').hide();
 
-  api.post('obadmanager','save_settings',{'enabled': enabled_id, 'disabled': disabled_id},function(response)
+  api.post('obadmanager','save_settings',{'enabled': enabled_id, 'disabled': disabled_id, 'timezone': timezone},function(response)
   {
     $('#ob_ad_manager_messagebox').text(response.msg).show();
   });
