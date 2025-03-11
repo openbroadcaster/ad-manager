cc7ae441be82e90c1301b74e648feaa3f8cc907b
Sanae <92342730+hinanaya@users.noreply.github.com>
2025-03-11 21:39:18 +0000

    cron support

---
+++ cron/adjustmedia.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace OB\Modules\ObAdManager\Cron;
+
+use OB\Classes\Base\Cron;
+
+class AdjustMedia extends Cron
+{
+    public function interval(): int
+    {
+        return 60 * 60 * 12;
+    }
+
+    public function run(): bool
+    {
+        echo "Adjusting media items.\n";
+
+        $models = \OBFModels::get_instance();
+        $models->obadmanager('adjust_media');
+
+        return true;
+    }
+}
